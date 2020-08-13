c25eea86f0186df141dd9e4a397eb4dee112de0e
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2020-08-13 09:35:59 +0100

    Routing

--- src/components/menu-item.js/menuItem.js
+++ src/components/menu-item.js/menuItem.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menuItem.scss';
 
 function MenuItem (props) {
-    return (
-      <div className={`${props.size} menu-item`} >
-        <div 
-          className="background-image"
-          style={{backgroundImage: `url(${props.imageUrl})`}}
-        />
-        <div className="content">
-          <h1 className="title">{props.title.toUpperCase()}</h1>
-          <span className="subtitle">Shop Now!!</span>
-        </div>
+  const handleClick = () => {
+   return props.history.push(`${props.match.url}${props.linkUrl}`);
+  }
+
+  return (
+    <div className={`${props.size} menu-item`} onClick={handleClick}  >
+      <div 
+        className="background-image"
+        style={{backgroundImage: `url(${props.imageUrl})`}}
+      />
+      <div className="content">
+        <h1 className="title">{props.title.toUpperCase()}</h1>
+        <span className="subtitle">Shop Now!!</span>
       </div>
-    )
+    </div>
+  )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homePage.js
+++ src/pages/homepage/homePage.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './homePage.scss';
 import Directory from '../../components/directory/directory';
 
-function HomePage () {
+function HomePage (props) {
+  console.log(props);
     return (
         <div className="homepage">
           <Directory />          
