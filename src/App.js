72cf1bd665d4e199295b7e3441045bc064f0d039
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2020-08-27 12:05:29 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/homePage';
 import ShopPage from './pages/shop/shopPage';
+import Header from './components/header/header';
+import SignInandSignUpPage from './pages/signInandSignUp/signInandSignUp';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/> 
-        <Route path="/shop" component={ShopPage} />            
+        <Route path="/shop" component={ShopPage} />  
+        <Route path="/signIn" component={SignInandSignUpPage} />            
       </Switch>      
     </div>
   );
---
+++ src/pages/signInandSignUp/signInandSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './signInandSignUp.scss';
+import SignIn from '../../components/sign-in/sign-in';
+
+function SignInandSignUpPage() {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInandSignUpPage;
